50b4fcff2d75219487418ba4ff5042ea81796eb9
Sai Pradeepti <saipradeepti14@gmail.com>
2021-04-15 19:05:18 +0530

    Adding Starter files of FAQ Accordion Challenge

---
+++ 03faq-accordion-card-main/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Faq from './Faq'
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="svg">
+        <img src="./img/bg-pattern-desktop.svg" alt="" style={{height: '100px',width:'100px'}}/>
+      </div>      
+      <Faq />      
+    </div>
+  )
+}
+
+export default App
