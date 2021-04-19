540a012642a076c862c42cf4a2442c0facfe447b
Sai Pradeepti <saipradeepti14@gmail.com>
2021-04-20 01:09:52 +0530

    Adjusting logo background

--- 04job-listings-with-filtering/src/Components/JobPost.js
+++ 04job-listings-with-filtering/src/Components/JobPost.js
@@ -3,11 +3,12 @@ import JobDetails from './JobDetails'
 import JobTags from './JobTags'
 
 const JobPost = ({id,company,logo,newpost,featured,position,role,level,postedAt,contract,location,languages,tools}) => {
+    console.log(logo)
     const jobDetailsInfo = {company,newpost,featured,position,postedAt,contract,location};
     const jobTagsInfo = {role,level,languages,tools};
     return (
         <div className={featured ? 'job job__border' : 'job'}>
-            <div className="job__logo">
+            <div className="job__logo" style={{background: `url(${logo})`,backgroundRepeat: 'no-repeat',backgroundPosition: 'center'}}>
                 {/* <div className="logo__img"></div> */}
             </div>
             <JobDetails {...jobDetailsInfo}/>
