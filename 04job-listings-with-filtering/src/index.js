96d515eac7b389c5b2e437d785c2379a9ab5accd
Sai Pradeepti <saipradeepti14@gmail.com>
2021-04-19 12:52:17 +0530

    Adding FrontEndMentor Design Files

--- 04job-listings-with-filtering/src/index.js
+++ 04job-listings-with-filtering/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './scss/main.scss'
-import App from './Components/App';
+import App from './components/App';
 
 ReactDOM.render(
   <React.StrictMode>
