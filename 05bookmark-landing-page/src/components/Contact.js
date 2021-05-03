4c7ca31969933c8e875759c27be41a4895099e8c
Sai Pradeepti <saipradeepti14@gmail.com>
2021-05-03 14:23:53 +0530

    Contact Section of Bookmark Landing Page

--- 05bookmark-landing-page/src/components/Contact.js
+++ 05bookmark-landing-page/src/components/Contact.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React,{ useState } from 'react'
 
 const Contact = () => {
+    const [email,setEmail] = useState('');
+    const [emailList,setEmailList] = useState([]);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setEmailList((emailList)=>{
+            if(email!=='')
+            return [...emailList,email];
+            else
+            return [...emailList];
+        })
+        console.log(emailList);
+        setEmail('');
+    }
     return (
-        <div>
-            <h1>Contact</h1>
+        <div className='contact'>
+            <div className='contact__title'>35,000+ already joined</div>
+            <div className='contact__content'>Stay up-to-date with what we’re doing</div>
+            <form className='contact__form' onSubmit={handleSubmit}>
+                <div className="form-control">
+                    <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)} placeholder='Enter your email address' />
+                </div>
+                <button className='contact__btn'>Contact Us</button>
+            </form>
         </div>
     )
 }
