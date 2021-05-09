015e2237a07b9c8ff05c89d67f5110a513c94da5
Sai Pradeepti <saipradeepti14@gmail.com>
2021-05-09 21:42:04 +0530

    Adding App Header

--- 06todo-app-main/src/components/App.js
+++ 06todo-app-main/src/components/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import '../scss/main.scss'
+import { FiSun } from "react-icons/fi";
+import { FiMoon } from "react-icons/fi"
 
 const App = () => {
+  const [lightTheme,setLightTheme] = useState(false);
   return (
-    <div>
-      Hello
+    <div className={`app ${lightTheme ? 'lightTheme' : 'darkTheme'}`}>
+      <div className="app__header">
+        <div className="app__title">todo</div>
+        <div className="app__darkTheme" onClick={()=>setLightTheme(!lightTheme)}>
+          {
+            lightTheme ? <FiMoon size={30} /> : <FiSun size={30} />
+          }
+        </div>
+      </div>
     </div>
   )
 }
