150277a103543c7363b6aaf44cbb21b2272e3c38
Sai Pradeepti <saipradeepti14@gmail.com>
2021-05-17 01:52:34 +0530

    Adding Animation to Navbar Overlay

--- 05bookmark-landing-page/src/components/Navbar.js
+++ 05bookmark-landing-page/src/components/Navbar.js
@@ -1,13 +1,14 @@
 import React,{ useState } from 'react'
+import Zoom from 'react-reveal/Zoom';
 
 const Navbar = () => {
     const [showOverlay,setShowOverlay] = useState(false);
 
     const overlay = () => {
         setShowOverlay(!showOverlay);
-    }
+    } 
 
-    return (        
+    return (
         <nav className='nav'>
             {
                 !showOverlay && (
@@ -38,37 +39,41 @@ const Navbar = () => {
                 }                
             </div>
             {
-                showOverlay && (
-                <div className="nav__overlay overlay">
-                <div className="overlay__menu">
-                    <div className="overlay__logo">
-                        <div className="logo">                        
-                            <img src="/img/logo-bookmark-overlay.svg" alt="logo-overlay" />
+                showOverlay && 
+                (
+                    <Zoom duration={1500}> 
+                        <div className="nav__overlay overlay">
+                
+                            <div className="overlay__menu">
+                                <div className="overlay__logo">
+                                    <div className="logo">                        
+                                        <img src="/img/logo-bookmark-overlay.svg" alt="logo-overlay" />
+                                    </div>
+                                    <div className="closeIcon" onClick={overlay}>
+                                        <img src="/img/icon-close.svg" alt="close-icon" />
+                                    </div>
+                                </div>
+                                <div className="overlay__links">
+                                    <div className="link">
+                                        <a href="#features" onClick={overlay}>features</a>
+                                    </div>
+                                    <div className="link">
+                                        <a href="#" onClick={overlay}>pricing</a>
+                                    </div>
+                                    <div className="link">
+                                        <a href="#contact" onClick={overlay}>contact</a>
+                                    </div>
+                                    <div className="link">
+                                        <a href="#" className="overlay-btn">login</a>
+                                    </div>
+                                </div>
+                                <div className="overlay__social">
+                                    <img src="/img/icon-facebook.svg" alt="facebook-icon" />
+                                    <img src="/img/icon-twitter.svg" alt="twitter-icon" />
+                                </div>
+                            </div>        
                         </div>
-                        <div className="closeIcon" onClick={overlay}>
-                            <img src="/img/icon-close.svg" alt="close-icon" />
-                        </div>
-                    </div>
-                    <div className="overlay__links">
-                        <div className="link">
-                            <a href="#features" onClick={overlay}>features</a>
-                        </div>
-                        <div className="link">
-                            <a href="#" onClick={overlay}>pricing</a>
-                        </div>
-                        <div className="link">
-                            <a href="#contact" onClick={overlay}>contact</a>
-                        </div>
-                        <div className="link">
-                            <a href="#" className="overlay-btn">login</a>
-                        </div>
-                    </div>
-                    <div className="overlay__social">
-                        <img src="/img/icon-facebook.svg" alt="facebook-icon" />
-                        <img src="/img/icon-twitter.svg" alt="twitter-icon" />
-                    </div>
-                </div>
-            </div>
+                    </Zoom> 
                 )
             }
             
