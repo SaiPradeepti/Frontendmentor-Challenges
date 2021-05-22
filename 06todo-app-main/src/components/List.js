dab06507f53bbc48822b7b332dcbf57690d77227
Sai Pradeepti <saipradeepti14@gmail.com>
2021-05-22 12:49:40 +0530

    Updating app__list for empty list state

--- 06todo-app-main/src/components/List.js
+++ 06todo-app-main/src/components/List.js
@@ -33,37 +33,43 @@ const List = ({dispatch,list,lightTheme}) => {
         setItemsLeft(count);
     }, [list])
     
+    // if(newList.length!==0)
     return (
-        <div className={`app__list ${lightTheme ? 'app__light' : false} ${newList.length===0? 'app__emptylist' : false}`}>
-            <Droppable droppableId="droppable">
-                {(provided, _) => (
-                    <div {...provided.droppableProps} ref={provided.innerRef} className={`todo__list ${newList.length === 0 ? 'emptyList' : false}`}>
-                        {(newList.length===0) && <div className='todo__msg'>Add items to the list!</div>}
-                        {   newList.map((item,index) => {
-                                    return (
-                                        <Draggable draggableId={item.id} index={index} key={item.id}>
-                                            {(provided, snapshot) => (
-                                                <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-                                                    <Todo item={item}  dispatch={dispatch} lightTheme={lightTheme} />
-                                                </div>
-                                            )}
-                                        </Draggable>
-                                    )                    
-                                })
-                        }                           
-                        {provided.placeholder}
-                    </div>
-                )}            
-            </Droppable>
+        <div className={`app__list ${lightTheme ? 'app__light' : false} ${newList.length===0 ? 'app__emptylist': false}`}>
+            {
+                (newList.length===0) && <div className='todo__msg'>Add items to the list!</div>
+            }
+            {                
+                (newList.length!==0) &&                 
+                <Droppable droppableId="droppable">
+                    {(provided, _) => (
+                        <div {...provided.droppableProps} ref={provided.innerRef} className="todo__list">
+                            {   newList.map((item,index) => {
+                                        return (
+                                            <Draggable draggableId={item.id} index={index} key={item.id}>
+                                                {(provided, snapshot) => (
+                                                    <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                                                        <Todo item={item}  dispatch={dispatch} lightTheme={lightTheme} />
+                                                    </div>
+                                                )}
+                                            </Draggable>
+                                        )                    
+                                    })
+                            }                           
+                            {provided.placeholder}
+                        </div>
+                    )}            
+                </Droppable>               
+            }
             <div className="app__menu">
-                <div className="app__listItems">{itemsLeft} items left</div>
-                <div className="app__states">
-                    <div className={`all ${activeState === 'all' ? 'activeState' : false}`} onClick={()=>setActiveState('all')}>All</div>
-                    <div className={`active ${activeState === 'active' ? 'activeState' : false}`} onClick={()=>setActiveState('active')}>Active</div>
-                    <div className={`completed ${activeState === 'completed' ? 'activeState' : false}`} onClick={()=>setActiveState('completed')}>Completed</div>
-                </div>
-                <div className="app__clearCompleted" onClick={()=>dispatch({type:'clearCompleted'})}>Clear Completed</div>
-            </div>
+                        <div className="app__listItems">{itemsLeft} items left</div>
+                        <div className="app__states">
+                            <div className={`all ${activeState === 'all' ? 'activeState' : false}`} onClick={()=>setActiveState('all')}>All</div>
+                            <div className={`active ${activeState === 'active' ? 'activeState' : false}`} onClick={()=>setActiveState('active')}>Active</div>
+                            <div className={`completed ${activeState === 'completed' ? 'activeState' : false}`} onClick={()=>setActiveState('completed')}>Completed</div>
+                        </div>
+                        <div className="app__clearCompleted" onClick={()=>dispatch({type:'clearCompleted'})}>Clear Completed</div>
+                    </div>
         </div>
     )
 }
