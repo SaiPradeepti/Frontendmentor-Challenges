ef1a8ab0f8b1c1055ca96cb1f2cdce6068d01766
Sai Pradeepti <saipradeepti14@gmail.com>
2021-05-26 14:39:09 +0530

    refactoring code using useContext and Context API

--- 06todo-app-main/src/components/App.js
+++ 06todo-app-main/src/components/App.js
@@ -1,42 +1,23 @@
-import React, { useEffect, useReducer } from 'react'
-import { reducer } from './reducer'
+import React, { useEffect } from 'react'
 import '../scss/main.scss'
 import Input from './Input'
 import List from './List'
 import Alert from './Alert'
 import { DragDropContext } from 'react-beautiful-dnd';
+import { useGlobalContext } from './context'
 
-const setLocalStorage = (list) => {
-  localStorage.setItem('list',JSON.stringify(list));
-}
-
-const getLocalStorage = () => {
-  let list = JSON.parse(localStorage.getItem('list'));
-  if(list)
-    return list;
-  else
-    return [];
-}
-
-const initialState = {
-  list: getLocalStorage(),
-  lightTheme: false,
-  showAlert: false,
-  alertContent: '',
-  alertColor: ''
-}
 
 const App = () => {
-  const [state,dispatch] = useReducer(reducer,initialState);
+  const { dispatch, lightTheme, list, setLocalStorage } = useGlobalContext();
 
   useEffect(() => {
-    setLocalStorage(state.list)
-  },[state.list]);
+    setLocalStorage(list)
+  },[list]);
     
 
   return (
     
-      <div className={`app ${state.lightTheme ? 'lightTheme' : 'darkTheme'}`}>
+      <div className={`app ${lightTheme ? 'lightTheme' : 'darkTheme'}`}>
         <DragDropContext onDragEnd={(param)=>{
           if(!param.destination){
             return;
@@ -44,23 +25,23 @@ const App = () => {
           const srcIndex = param.source.index;
           const destIndex = param.destination.index;
 
-          state.list.splice(destIndex,0,state.list.splice(srcIndex,1)[0]);
-          setLocalStorage(state.list);
+          list.splice(destIndex,0,list.splice(srcIndex,1)[0]);
+          setLocalStorage(list);
           console.log(param);
         }}>
         {
-          <Alert dispatch={dispatch} content={state.alertContent} color={state.alertColor} showAlert={state.showAlert}/>
+          <Alert />
         }
         <div className="app__header">
           <div className="app__title">todo</div>
           <div className="app__darkTheme" onClick={()=>dispatch({type:'toggleLightTheme'})}>
             {
-              state.lightTheme ? <img src="./images/icon-moon.svg" alt="moon" /> : <img src="./images/icon-sun.svg" alt="sun" />
+              lightTheme ? <img src="./images/icon-moon.svg" alt="moon" /> : <img src="./images/icon-sun.svg" alt="sun" />
             }
           </div>
         </div>
-        <Input dispatch={dispatch} lightTheme={state.lightTheme} />
-        <List dispatch={dispatch} list={state.list} lightTheme={state.lightTheme} />
+        <Input />
+        <List  />
         <div className="app__drag">Drag and drop to reorder list</div>        
         </DragDropContext>
       </div>
