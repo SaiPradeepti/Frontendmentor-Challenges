24d729aa81e8f76054d50c64e6f6578390ed945c
Sai Pradeepti <saipradeepti14@gmail.com>
2022-02-12 20:23:35 +0530

    Adding countries to Main Component

--- 07-rest-countries-api-with-color-theme-switcher/src/components/Main.js
+++ 07-rest-countries-api-with-color-theme-switcher/src/components/Main.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import '../sass/main.scss'
+import { useGlobalcontext } from './context'
 
 const Main = () => {
+  const {state, dispatch} = useGlobalcontext();
+  
   return (
-    <div></div>
+    <main className="main">
+      <div className="main__container">
+        {state.data.map((item) => {
+          const {name, flags: {png:img}, population, region, capital} = item;
+          
+          return (
+            <div className="card">
+              <div className="card__img">
+                <img src={img} alt={name} />
+              </div>
+              <div className="card__info">
+                <div className='card__name'>{name}</div>
+                <div className='country__info'>
+                  <div className='card__population'>population: <span className="countryInfovalue">{population}</span></div>
+                <div className='card__region'>region: <span className="countryInfovalue">{region}</span></div>
+                <div className='card__capital'>capital: <span className="countryInfovalue">{capital}</span></div>
+                </div>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    </main>
   )
 }
 
